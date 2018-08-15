a91fdc20549c5b98901d4d994604061af53f39cb
Billy Vong <billy@sentry.io>
2018-08-14 18:10:12 -0700

    wip: add Browsers and OS

--- src/sentry/static/sentry/app/routes.jsx
+++ src/sentry/static/sentry/app/routes.jsx
@@ -746,6 +746,7 @@ function routes() {
                 import(/*webpackChunkName: HealthOverview*/ './views/organizationHealth/overview')}
               component={errorHandler(LazyLoad)}
             />
+
             <Route
               path="errors"
               componentPromise={() =>
@@ -757,6 +758,13 @@ function routes() {
               path="transactions"
               componentPromise={() =>
                 import(/*webpackChunkName: HealthTransactions*/ './views/organizationHealth/transactions')}
+              component={errorHandler(LazyLoad)}
+            />
+
+            <Route
+              path="browsers"
+              componentPromise={() =>
+                import(/*webpackChunkName: HealthErrors*/ './views/organizationHealth/browsers')}
               component={errorHandler(LazyLoad)}
             />
           </Route>
